07e43ca73fee923c5d836a913c9112e99d32f0d0
Abdalla Ahmed Talaat <b.abdallaahmed@gmail.com>
2021-11-20 21:56:15 +0200

    version 1.1

--- C1_J.py
+++ C1_J.py
@@ -1,7 +1,17 @@
 while True:
+
     try:
         c1 = float(input("\n\nINPUT C1 Value: "))
-        print("\nJ= {:.4f}".format(1.25 * (1 - (1 - 3.2 * (1 / c1 ** 2)) ** 0.5)))
+        try:
+            r = 1/c1**2
+            c_d = 1.25 * (1 - (1 - 4.48*r) ** 0.5)
+            j = (1-0.4*c_d)/1.15
 
-    except:
-        print("BAD INPUT")
+            print("\nR= {:.4f}".format(r))
+            print("\nc/d= {:.4f}".format(c_d))
+
+            if j > 0.826: j = 0.826
+            print("\nJ= {:.4f}".format(j))
+        except: print("\nC1 value too small!")
+
+    except: print("BAD INPUT")
