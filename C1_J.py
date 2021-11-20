5c03e2a9ce3ee8cf655a12172329c6d73f60b3cc
Abdalla Ahmed Talaat <b.abdallaahmed@gmail.com>
2021-11-20 22:29:22 +0200

    Version 1.2.2
    added warning messages for small C1

--- C1_J.py
+++ C1_J.py
@@ -2,6 +2,16 @@ while True:
 
     try:
         c1 = float(input("\n\n\nINPUT C1 Value: "))
+        if c1 <= 2.92:
+            print("This C1 Value is small. Make sure it satisfies your minimum C1 according to fy:")
+            print("""
+fy = 500 \n>> C1_min = 2.92
+fy = 420 \n>> C1_min = 2.86
+fy = 400 \n>> C1_min = 2.83
+fy = 350 \n>> C1_min = 2.75
+fy = 240 \n>> C1_min = 2.65
+            
+            """)
         try:
             r = 1/c1**2
             c_d = 1.25 * (1 - (1 - 4.48*r) ** 0.5)
